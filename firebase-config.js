13e2dba1c1c02a4083216b9256f0b5373ebdf555
tanoojreddy1801-rgb <tanoojreddy1801@gmail.com>
2026-04-25 00:34:54 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCsxAw40sFLw00PtEas3cvzRMEjkDZ9NJE",
+  authDomain: "client-tracker-tool-5bdf6.firebaseapp.com",
+  projectId: "client-tracker-tool-5bdf6"
+};
+
+export const FIRESTORE_COLLECTION = "refundRecords";
+export const FIRESTORE_METADATA_DOC = "datasetMeta/current";
